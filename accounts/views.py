a67df13d83ad28d2ef532431bca61a9b38fc03f4
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-03-14 17:23:07 +0200

    Use lookup_field for UserDetailsView

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.http import Http404
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, get_object_or_404, UpdateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from accounts.models import UserRoles
@@ -32,11 +31,7 @@ class UserDetailsAPIView(RetrieveAPIView):
     permission_classes = [IsAccountOwner | IsAdmin]
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-    def get_object(self):
-        obj = get_object_or_404(self.queryset, uuid=self.kwargs.get('uuid'))
-        self.check_object_permissions(self.request, obj)
-        return obj
+    lookup_field = 'uuid'
 
 
 class UserCreateAPIView(CreateAPIView):
