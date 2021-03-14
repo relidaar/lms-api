6f8771bafe2ea76344f4dcdbeeff3589e931fd8e
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-03-14 16:42:55 +0200

    Add tests for UserDetailsView

--- accounts/tests.py
+++ accounts/tests.py
@@ -143,3 +143,57 @@ class GetAllUsersTests(TestCase):
         """Test if not authenticated user can retrieve users list."""
         response = self.client.get(self.get_users_list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class GetSingleUserTests(TestCase):
+    """Test module for user CRUD views."""
+    urlpatterns = test_urlpatterns
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            full_name='Jane Doe',
+            email=user_login_data['email'],
+            password=user_login_data['password'],
+            role=UserRoles.STUDENT,
+        )
+
+        self.admin = User.objects.create_superuser(
+            full_name='John Doe',
+            email=admin_login_data['email'],
+            password=admin_login_data['password'],
+        )
+
+        self.users = [self.user, self.admin]
+        self.login_url = reverse('rest_login')
+
+    def test_admin_get_valid_single_user(self):
+        """Test if admin can retrieve valid single user."""
+        self.client.post(self.login_url, admin_login_data)
+        for user_to_test in self.users:
+            response = self.client.get(reverse('get_user', kwargs={'uuid': user_to_test.uuid}))
+            user = User.objects.get(uuid=user_to_test.uuid)
+            serializer = UserSerializer(user)
+            self.assertEqual(response.data, serializer.data)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_admin_get_invalid_single_user(self):
+        """Test if admin can retrieve invalid single user."""
+        self.client.post(self.login_url, admin_login_data)
+        response = self.client.get(reverse('get_user', kwargs={'uuid': uuid4()}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_get_account_details(self):
+        """Test if user can retrieve account details."""
+        self.client.post(self.login_url, user_login_data)
+        response = self.client.get(reverse('get_user', kwargs={'uuid': self.user.uuid}))
+        user = User.objects.get(uuid=self.user.uuid)
+        serializer = UserSerializer(user)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_get_another_account_details(self):
+        """Test if user can retrieve another account details."""
+        self.client.post(self.login_url, user_login_data)
+        response = self.client.get(reverse('get_user', kwargs={'uuid': self.admin.uuid}))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
