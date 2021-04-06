3db771afbceffd2113120a667cfd62b7d6182a2f
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-04-06 21:33:16 +0300

    Add basic LMS models

--- lms_core/admin.py
+++ lms_core/admin.py
@@ -1,1 +1,57 @@
 from django.contrib import admin
+
+from lms_core.models import Course, StudentGroup, Timetable, PeriodicEvent, NonPeriodicEvent, EventType
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title', 'uuid',)
+    search_fields = ('code', 'title', 'uuid',)
+    ordering = ('code',)
+
+
+@admin.register(StudentGroup)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'uuid',)
+    search_fields = ('code', 'uuid',)
+    ordering = ('code',)
+
+
+@admin.register(Timetable)
+class TimetableAdmin(admin.ModelAdmin):
+    list_display = ('get_course_code', 'uuid',)
+    search_fields = ('get_course_code', 'uuid',)
+
+    def get_course_code(self, obj):
+        return obj.course.code
+    get_course_code.short_description = 'Course Code'
+    get_course_code.admin_order_field = 'code'
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_type', 'get_instructor', 'uuid',)
+    search_fields = ('title', 'event_type', 'get_instructor', 'uuid',)
+    list_filter = ('event_type',)
+
+    def get_instructor(self, obj):
+        return obj.instructor.full_name
+    get_instructor.short_description = 'Instructor'
+    get_instructor.admin_filter_field = 'full_name'
+
+
+@admin.register(PeriodicEvent)
+class PeriodicEventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NonPeriodicEvent)
+class NonPeriodicEventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uuid',)
+    search_fields = ('title', 'uuid',)
+    ordering = ('title',)
+
--- lms_core/models.py
+++ lms_core/models.py
@@ -1,1 +1,89 @@
+import uuid
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from accounts.models import InstructorProfile, StudentProfile
+
+
+class StudentGroup(models.Model):
+    """Represent an academic group of students."""
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    code = models.CharField(max_length=10, unique=True)
+    students = models.ManyToManyField(StudentProfile)
+
+
+class Course(models.Model):
+    """Represent an academic course."""
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    code = models.CharField(max_length=10, unique=True)
+    title = models.CharField(max_length=255)
+    syllabus = models.TextField()
+    instructors = models.ManyToManyField(InstructorProfile, related_name='instructors')
+    student_groups = models.ManyToManyField(StudentGroup)
+
+    def __str__(self):
+        return f'Course {self.code} - {self.title}'
+
+
+class Timetable(models.Model):
+    """Represent an event timetable related to specific course."""
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='timetables')
+    start_date = models.DateField(verbose_name='Course start date')
+    end_date = models.DateField(verbose_name='Course end date')
+
+    def __str__(self):
+        return f'Timetable for course {self.course.code}'
+
+
+class EventType(models.Model):
+    """Represent types of timetable events."""
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    """Basic model for timetable events."""
+    uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    event_type = models.OneToOneField(EventType, on_delete=models.CASCADE)
+    start_time = models.TimeField(verbose_name='Event start time')
+    end_time = models.TimeField(verbose_name='Event end time')
+    instructor = models.ForeignKey(InstructorProfile, on_delete=models.CASCADE)
+    students = models.ManyToManyField(StudentProfile)
+    timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Event created by {self.instructor}'
+
+    class Meta:
+        abstract = True
+
+
+class NonPeriodicEvent(Event):
+    """Represent non periodic event with specific date."""
+    date = models.DateField()
+
+
+class PeriodicEvent(Event):
+    """Represent periodic event with specific weekday and repeat type."""
+    class WeekDay(models.TextChoices):
+        Monday = 'MO', _('Monday')
+        Tuesday = 'TU', _('Tuesday')
+        Wednesday = 'WE', _('Wednesday')
+        Thursday = 'TH', _('Thursday')
+        Friday = 'FR', _('Friday')
+        Saturday = 'SA', _('Saturday')
+        Sunday = 'SU', _('Sunday')
+
+    class RepeatType(models.TextChoices):
+        Weekly = 'W', _('Weekly')
+        Even = 'E', _('Even')
+        Odd = 'O', _('Odd')
+
+    weekday = models.CharField(max_length=2, choices=WeekDay.choices, default=WeekDay.Monday)
+    repeat_type = models.CharField(max_length=1, choices=RepeatType.choices, default=RepeatType.Weekly)
