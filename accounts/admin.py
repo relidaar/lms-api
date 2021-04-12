c1ab2f3322345ee0b12a2fd80fd13c3c6875d152
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-04-12 17:55:30 +0300

    Fix CustomUser form for admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,10 +13,14 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('full_name', 'email', 'uuid',)
     ordering = ('full_name',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('full_name',)}),
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('Personal info'), {
+            'fields': ('full_name',)
+        }),
         (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'fields': ('is_staff', 'groups',),
         }),
     )
     add_fieldsets = (
@@ -25,14 +29,14 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('email', 'full_name', 'password1', 'password2'),
         }),
         (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'fields': ('is_staff', 'groups',),
         }),
     )
 
     def get_exclude(self, request, obj=None):
         excluded = super().get_exclude(request, obj) or []
-        if not request.user.is_superuser:
-            excluded += ('is_active', 'is_superuser', 'user_permissions',)
+        if not request.admin.is_superuser:
+            return excluded + ['is_active', 'is_superuser', 'user_permissions']
         return excluded
 
 
@@ -41,13 +45,13 @@ class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ('get_full_name', 'get_email', 'uuid',)
 
     def get_full_name(self, obj):
-        return obj.user.full_name
+        return obj.admin.full_name
 
     get_full_name.short_description = 'Full Name'
     get_full_name.admin_order_field = 'full_name'
 
     def get_email(self, obj):
-        return obj.user.email
+        return obj.admin.email
 
     get_email.short_description = 'Email'
 
