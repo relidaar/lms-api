1f030b0728af35e8d539f5715b832c35b4ffbdbb
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-04-13 10:56:11 +0300

    Update url variables naming

--- lms_core/tests.py
+++ lms_core/tests.py
@@ -47,8 +47,8 @@ class StudentGroupCrudTests(APITestCase):
 
         self.login_url = reverse('rest_login')
         self.logout_url = reverse('rest_logout')
-        self.student_groups_list_url = reverse('student-group-list')
-        self.student_group_detail_url = reverse('student-group-detail', kwargs={'uuid': self.student_group1.uuid})
+        self.list_url = reverse('student-group-list')
+        self.detail_url = reverse('student-group-detail', kwargs={'uuid': self.student_group1.uuid})
 
     def test_get_all_student_groups(self):
         """Test if superuser can retrieve student groups list."""
@@ -57,7 +57,7 @@ class StudentGroupCrudTests(APITestCase):
         self.student_group2 = StudentGroup.objects.create(code='group2')
         self.student_group2.students.add(self.student2)
 
-        response = self.client.get(self.student_groups_list_url)
+        response = self.client.get(self.list_url)
         student_groups = StudentGroup.objects.all()
         serializer = StudentGroupSerializer(student_groups, many=True)
         self.assertEqual(response.data, serializer.data)
@@ -65,13 +65,13 @@ class StudentGroupCrudTests(APITestCase):
 
     def test_get_all_student_groups_not_authenticated(self):
         """Test if not authenticated user can retrieve student groups list."""
-        response = self.client.get(self.student_groups_list_url)
+        response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_valid_single_student_group(self):
         """Test if superuser can retrieve valid student group details."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.get(self.student_group_detail_url)
+        response = self.client.get(self.detail_url)
         group = StudentGroup.objects.get(uuid=self.student_group1.uuid)
         serializer = StudentGroupSerializer(group)
         self.assertEqual(response.data, serializer.data)
@@ -79,7 +79,7 @@ class StudentGroupCrudTests(APITestCase):
 
     def test_get_single_student_group_not_authenticated(self):
         """Test if not authenticated user can retrieve student group details."""
-        response = self.client.get(self.student_group_detail_url)
+        response = self.client.get(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_invalid_single_student_group(self):
@@ -96,18 +96,18 @@ class StudentGroupCrudTests(APITestCase):
             'code': 'group2',
             'students': [self.student2.pk]
         }
-        response = self.client.post(self.student_groups_list_url, data)
+        response = self.client.post(self.list_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_invalid_student_group(self):
         """Test if superuser can create invalid student group."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.post(self.student_groups_list_url)
+        response = self.client.post(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_student_group_not_authenticated(self):
         """Test if not authenticated user can create new student group."""
-        response = self.client.post(self.student_groups_list_url)
+        response = self.client.post(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_valid_update_student_group_details(self):
@@ -118,31 +118,31 @@ class StudentGroupCrudTests(APITestCase):
             'code': 'group51',
             'students': [self.student1.pk]
         }
-        put_response = self.client.put(self.student_group_detail_url, data)
-        patch_response = self.client.patch(self.student_group_detail_url, {'code': 'group41'})
+        put_response = self.client.put(self.detail_url, data)
+        patch_response = self.client.patch(self.detail_url, {'code': 'group41'})
         self.assertEqual(put_response.status_code, status.HTTP_200_OK)
         self.assertEqual(patch_response.status_code, status.HTTP_200_OK)
 
     def test_invalid_update_student_group_details(self):
         """Test if superuser can invalid update student group details."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.put(self.student_group_detail_url)
+        response = self.client.put(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_student_group_details_not_authenticated(self):
         """Test if not authenticated user can update student group details."""
-        response = self.client.put(self.student_group_detail_url)
+        response = self.client.put(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_student_group(self):
         """Test if superuser can delete student group."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.delete(self.student_group_detail_url)
+        response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_student_group_not_authenticated(self):
         """Test if not authenticated user can delete student group."""
-        response = self.client.delete(self.student_group_detail_url)
+        response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
@@ -185,8 +185,8 @@ class CourseCrudTests(APITestCase):
 
         self.login_url = reverse('rest_login')
         self.logout_url = reverse('rest_logout')
-        self.course_list_url = reverse('course-list')
-        self.course_detail_url = reverse('course-detail', kwargs={'uuid': self.course.uuid})
+        self.list_url = reverse('course-list')
+        self.detail_url = reverse('course-detail', kwargs={'uuid': self.course.uuid})
 
     def test_get_all_courses(self):
         """Test if superuser can retrieve courses list."""
@@ -196,31 +196,31 @@ class CourseCrudTests(APITestCase):
         self.course2.instructors.add(self.instructor)
         self.course2.student_groups.add(self.student_group)
 
-        response = self.client.get(self.course_list_url)
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
+        response = self.client.get(self.list_url)
+        data = Course.objects.all()
+        serializer = CourseSerializer(data, many=True)
 
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_all_courses_not_authenticated(self):
         """Test if not authenticated user can retrieve courses list."""
-        response = self.client.get(self.course_list_url)
+        response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_valid_single_course(self):
         """Test if superuser can retrieve valid course details."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.get(self.course_detail_url)
-        course = Course.objects.get(uuid=self.course.uuid)
-        serializer = CourseSerializer(course)
+        response = self.client.get(self.detail_url)
+        data = Course.objects.get(uuid=self.course.uuid)
+        serializer = CourseSerializer(data)
 
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_single_course_not_authenticated(self):
         """Test if not authenticated user can retrieve course details."""
-        response = self.client.get(self.course_detail_url)
+        response = self.client.get(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_invalid_single_course(self):
@@ -239,18 +239,18 @@ class CourseCrudTests(APITestCase):
             'instructors': [self.instructor.pk],
             'student_groups': [self.student_group.pk],
         }
-        response = self.client.post(self.course_list_url, data)
+        response = self.client.post(self.list_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_invalid_course(self):
         """Test if superuser can create invalid course."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.post(self.course_list_url)
+        response = self.client.post(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_course_not_authenticated(self):
         """Test if not authenticated user can create new course."""
-        response = self.client.post(self.course_list_url)
+        response = self.client.post(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_valid_update_course_details(self):
@@ -262,29 +262,29 @@ class CourseCrudTests(APITestCase):
             'instructors': [self.instructor.pk],
             'student_groups': [self.student_group.pk],
         }
-        put_response = self.client.put(self.course_detail_url, data)
-        patch_response = self.client.patch(self.course_detail_url, {'code': 'course5'})
+        put_response = self.client.put(self.detail_url, data)
+        patch_response = self.client.patch(self.detail_url, {'code': 'course5'})
         self.assertEqual(put_response.status_code, status.HTTP_200_OK)
         self.assertEqual(patch_response.status_code, status.HTTP_200_OK)
 
     def test_invalid_update_course_details(self):
         """Test if superuser can invalid update course details."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.put(self.course_detail_url)
+        response = self.client.put(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_course_details_not_authenticated(self):
         """Test if not authenticated user can update course details."""
-        response = self.client.put(self.course_detail_url)
+        response = self.client.put(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_course(self):
         """Test if superuser can delete course."""
         self.client.post(self.login_url, self.superuser_login_data)
-        response = self.client.delete(self.course_detail_url)
+        response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_course_not_authenticated(self):
         """Test if not authenticated user can delete course."""
-        response = self.client.delete(self.course_detail_url)
+        response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
