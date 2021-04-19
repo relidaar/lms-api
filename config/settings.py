b06ac84c9d98603e515fb570a78b7b9fadae155c
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-04-19 19:47:58 +0300

    Update Django to the latest version (v3.2)

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
