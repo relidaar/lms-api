65cee82a01f613b7e909685542fc24c79ef5af0e
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-04-23 17:23:42 +0300

    Add API version to urls

--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,14 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include([
+    path('api/v1/', include([
         path('accounts/', include('accounts.urls')),
         path('lms-core/', include('lms_core.urls')),
 
         path('schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-        path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+        path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'),
+             name='swagger'),
+        path('redoc/', SpectacularRedocView.as_view(url_name='schema'),
+             name='redoc'),
     ])),
 ]
