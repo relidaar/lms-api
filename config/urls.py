322cb525d4386ae1960d1a7c39c34ecc391962f5
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2021-05-06 14:38:57 +0300

    Disable admin panel

--- config/urls.py
+++ config/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/', include([
         path('accounts/', include('accounts.urls')),
         path('lms-core/', include('lms_core.urls')),
